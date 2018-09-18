a4f3cd1f2af7f2175afec7e84a78ce8e3a23309b
Lunik <guillaume.lunik@gmail.com>
2018-09-18 14:18:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
  * @param {!Object} req HTTP request context.
  * @param {!Object} res HTTP response context.
  */
-const Meteo = require('./lib/meteo.js')
 
 exports.meteo = (req, res) => {
   res.end("Hello World!")
